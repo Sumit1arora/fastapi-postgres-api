56af563dd5175ad69d50ac0fe9aa6b71967d3850
Sumit Arora <94929971+Sumit1arora@users.noreply.github.com>
2025-10-16 18:13:23 +0530

    Change database connection string
    Updated DATABASE_URL to new connection string.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user1:Sumit12@localhost:5432/fastapi_db"
+DATABASE_URL = "postgresql://user1:rh3cV9jcxbXcceEsVtOx0Kllnuo2XAom@dpg-d3oefcur433s73a1c9ug-a/fastapi_db_ip4q"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
